242ec252b729a3b0c2af5cce6a9cf128199830a5
Daniel Martinez Marin <dani21altea@gmail.com>
2024-03-14 13:59:54 +0100

    feat: refactor filter heroes

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,38 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroComponent } from './components/hero/hero.component';
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Hero } from './models/hero.model';
 import { HeroesService } from './services/heroes.service';
 import { Subject } from 'rxjs/internal/Subject';
-import { debounceTime, startWith, switchMap } from 'rxjs/operators';
-import { Observable } from 'rxjs/internal/Observable';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  startWith,
+} from 'rxjs/operators';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [NgIf, NgFor, AsyncPipe, HeroComponent],
+  imports: [
+    NgIf,
+    AsyncPipe,
+    HeroComponent,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent implements OnInit {
-  private searchHero = new Subject<string>();
-  heroes$!: Observable<Hero[]>;
+  heroes: Hero[] = [];
+  heroName = new FormControl<string | Hero>('');
+  filteredOptions?: Observable<Hero[]>;
 
   constructor(private _heroesService: HeroesService) {}
 
   ngOnInit(): void {
-    this.heroes$ = this.searchHero.pipe(
+    this._heroesService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes));
+
+    this.filteredOptions = this.heroName.valueChanges.pipe(
       startWith(''),
       debounceTime(300),
-      switchMap((heroName: string) =>
-        heroName
-          ? this._heroesService.searchHeroes(heroName)
-          : this._heroesService.getHeroes()
-      )
+      map((value) => {
+        console.log('valor', value);
+
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filter(name as string) : this.heroes.slice();
+      })
     );
   }
 
-  onSearch(term: string): void {
-    this.searchHero.next(term);
+  displayFn(hero: Hero): string {
+    return hero && hero.name ? hero.name : '';
+  }
+
+  private _filter(name: string): Hero[] {
+    const filterValue = name.toLowerCase();
+
+    return this.heroes.filter((hero) =>
+      hero.name.toLowerCase().includes(filterValue)
+    );
   }
 }
